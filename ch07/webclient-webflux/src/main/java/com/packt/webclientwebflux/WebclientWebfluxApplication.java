2cd10b37c3ecdfd3f839e6fa7353662e76e1da22
Kamila Santos <kahsantos89@gmail.com>
2023-02-22 23:20:16 -0300

    ch 7: base do exemplo

---
+++ ch07/webclient-webflux/src/main/java/com/packt/webclientwebflux/WebclientWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.packt.webclientwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclientWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebclientWebfluxApplication.class, args);
+	}
+
+}
