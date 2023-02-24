634aa7b565caf7696fc0128c1b2802a97e50cb51
Kamila Santos <kahsantos89@gmail.com>
2023-02-24 20:07:30 -0300

    reactive feign example

---
+++ ch07/reactive-feign-webflux/src/main/java/com/packt/reactivefeignwebflux/ReactiveFeignWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.packt.reactivefeignwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveFeignWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveFeignWebfluxApplication.class, args);
+	}
+
+}
